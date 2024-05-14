b93c46eaf3f885943a400441a02bdabcbb8d8433
Tatiana020 <l.rosalesp@uniandes.edu.co>
2024-05-13 21:54:51 -0500

    interfaz operador

--- src/galeria/controller_galeria/ControladorOperador.java
+++ src/galeria/controller_galeria/ControladorOperador.java
@@ -1,6 +1,8 @@
 package galeria.controller_galeria;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 import galeria.Galeria;
@@ -8,13 +10,13 @@ import galeria.structurer_inventario.Oferta;
 import galeria.structurer_inventario.Subasta;
 import galeria.structurer_inventario.Venta;
 import galeria.structurer_usuarios.Operador;
+import galeria.structurer_inventario.InventarioGaleria;
 
 public class ControladorOperador {
 	private Operador operador; 
 	private Galeria galeria;
 
 	public ControladorOperador(Galeria galeria, Operador operador){
-		this.operador = operador;
 		this.galeria = galeria;
 	}
 	public void agregarOfertaPendiente(int indice) {
@@ -25,6 +27,21 @@ public class ControladorOperador {
 	public void planearSubasta(Venta venta, double valorMinimo , LocalDateTime tiempo, double valorInicial) {
 		Subasta subasta = new Subasta(valorMinimo, valorInicial, tiempo, venta.getPieza(), null);
 		galeria.getInventarioGaleria().agregarSubasta(subasta, venta);
+	}
+
+	public List<Venta> getVentasPendientes(){
+		List<Venta> resultado =new ArrayList<Venta>();
+		for(Venta venta:galeria.getInventarioGaleria().getVentasPendientes().values()) {
+			if (venta.getComprador()==null) {
+				resultado.add(venta);
+			}
+		}
+		return resultado;
+	}
+	
+	
+	public void actualizarInfo(String contrasena, String nombre, String celular, String correo) {
+		operador.actualizarDatos(contrasena, nombre, celular, correo);
 	}
 	
 	public List<Oferta> getOfertasPendientes(){
---
+++ src/interfaz/InterfazOperador.java
@@ -0,0 +1,126 @@
+package interfaz;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+
+import galeria.Galeria;
+import galeria.controller_galeria.ControladorOperador;
+import galeria.controller_galeria.CoordinadorSesion;
+import galeria.structurer_inventario.Oferta;
+import galeria.structurer_inventario.Venta;
+import persistencia.PersistenciaNuevo;
+
+
+public class InterfazOperador extends InterfazAbstracta{
+	public static void main(String[] args) {
+		System.out.println("Bienvenido a la interfaz de operador!");
+		
+		//TODO Cargar la galeria ya guardada
+		PersistenciaNuevo persistenciaNuevo = new PersistenciaNuevo();
+		Galeria galeria = persistenciaNuevo.nuevaGaleria();
+		ControladorOperador cOperador = iniciarSesion(galeria);
+
+		int s =-1;
+		while(s != 5) {
+			mostrarOpciones();
+			s = inputInt("Seleccione una opcion:");
+			if(s == 1) {
+				uno(cOperador);
+			}
+			else if(s == 2) {
+				dos(cOperador);
+			}
+			
+			else if(s == 3) {
+				tres(cOperador);
+			}
+			else if(s == 4) {
+				cuatro(cOperador);
+			}
+
+			else if(s == 5) {
+				System.out.println("Saliendo...");
+			}
+			
+			else {
+				System.out.println("Opcion incorrecta, vuelva a intentarlo");
+			}
+		}
+		
+		}
+	public static ControladorOperador iniciarSesion(Galeria galeria){
+		ControladorOperador controladorOperador = null;
+			while(controladorOperador == null) {
+			String nombreUsuario = input("Ingrese su nombre de usuario (OPERADOR):");
+			String contrasena = input("Ingrese su contraseña (0000):");
+			CoordinadorSesion coordinadorSesion = new CoordinadorSesion(galeria, nombreUsuario, contrasena);
+			coordinadorSesion.iniciarSesion();
+			if(coordinadorSesion.getControladorActual().equals("ControladorOperador")) {
+				controladorOperador = coordinadorSesion.getControladorOperador();
+			}
+			else System.out.println("Usuario y/o contraseña incorrectos, vuelvalo a intentar");
+			}
+		return controladorOperador;
+	}
+	public static void mostrarOpciones() {
+		System.out.println("\n1- Agregar Oferta Pendiente");
+		System.out.println("2- Planear Subasta");
+		System.out.println("3- Actualizar datos");
+		System.out.println("4- Cerrar sesion");}
+	
+	public static void uno(ControladorOperador cOperador) {
+		List<Oferta> ofertasPendientes = cOperador.getOfertasPendientes();
+		if (ofertasPendientes.size()==0){
+			System.out.println("No tiene ofertas pendientes.");
+		}
+		else {
+			System.out.println("Indice/ Pieza/ Valor");
+			int i=0;
+			for(Oferta oferta:ofertasPendientes) {
+				System.out.println(Integer.toString(i)+"/ "+oferta.getSubasta().getPieza().getTitulo() 
+						+ "/ " + oferta.getValor());
+				i++;
+			}
+		i=inputIndex("Seleccione una opción: ", ofertasPendientes.size());
+		cOperador.agregarOfertaPendiente(i);
+		}
+		int indice = inputIndex("Ingrese el indice de la pieza a la : ",ofertasPendientes.size());
+		cOperador.agregarOfertaPendiente(indice);
+		System.out.println("Oferta agregada!");
+	}
+	public static void dos(ControladorOperador cOperador) {
+		List<Venta> ventas = cOperador.getVentasPendientes();
+		if(ventas.size()==0) {
+			System.out.println("No hay piezas para subastar");
+		}
+		else {
+			System.out.println("Indice/ Titulo");
+			int i=0;
+			for(Venta venta:ventas) {
+				System.out.println(Integer.toString(i)+"/ " + venta.getPieza().getTitulo());
+				i++;
+			}
+			i = inputIndex("Seleccione el índice de la pieza que desea subastar: ", i);
+			Venta venta= ventas.get(i);
+	        float valorMinimo = inputFloat("Ingrese el valor mínimo de la subasta: ");
+	        String fecha= input("Ingrese la fecha de finalización de la subasta (formato: yyyy-MM-dd):");
+	        LocalDateTime fechaFinal = LocalDateTime.parse(fecha, DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+	
+	        float valorInicial = inputFloat("Ingrese el valor inicial de la subasta:");
+	
+	        cOperador.planearSubasta(venta, valorMinimo, fechaFinal, valorInicial);
+		}
+	
+	}
+	public static void tres(ControladorOperador cOperador) {
+		String contrasena = input("Ingrese una nueva contraseña:");
+		String nombre = input("Ingrese su nombre:");
+		String correo =  input("Ingrese su correo:");
+		String celular =  input("Ingrese su numero de celular:");
+		cOperador.actualizarInfo(contrasena, nombre, celular, correo);
+		System.out.println("Actualizacion exitosa!");
+	}
+	public static void cuatro(ControladorOperador cOperador) {
+		//TODO cargar toda la informacion y actualizarla en los archivos de persistencia.
+	}
+	}
