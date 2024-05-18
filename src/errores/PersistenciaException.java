26df971bd68f10cb549749fd0229f5d863b5d8b3
RubiM2 <158978174+RubiM2@users.noreply.github.com>
2024-05-18 03:12:53 +0000

    persistencia2

--- src/errores/PersistenciaException.java
+++
@@ -1,9 +0,0 @@
-package errores;
-
-public class PersistenciaException extends Exception {
-	    private static final long serialVersionUID = 1L;
-
-		public PersistenciaException(String message, Throwable cause) {
-	        super(message, cause);
-	    }
-	}
